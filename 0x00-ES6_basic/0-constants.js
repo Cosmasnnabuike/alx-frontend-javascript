326d59285f96c1aae86ca57033087d306c431b23
Cosmasnnabuike <cosmasnnabuike@gmail.com>
2024-10-02 12:36:39 -0700

    1-block-scoped.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,12 +1,15 @@
-export function taskFirst () {
-	var task = 'I prefer const when i can.';
-	return task;
+export function taskFirst() {
+  let task = 'const.';
+  return task;
 }
-export function getLast () {
-	return ' is okay';
+
+export function getLast() {
+  return 'is okay';
 }
-export function taskNext () {
-	var combination = 'But sometimeslet';
-	combination += getLast ();
-	return combination;
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
